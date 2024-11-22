8638eb97670a14b6e0a498e11d5ecefc512b80e0
gabrielneeeves <gabrielnevesferreira08@gmail.com>
2024-11-22 18:10:02 -0300

    adicionar autenticacao para as rotas ALBUM e FOTO

--- src/main/java/com/fotoflow/fotoflowApi/infra/SecurityConfig.java
+++ src/main/java/com/fotoflow/fotoflowApi/infra/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.fotoflow.fotoflowApi.infra;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,8 +28,34 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/register").permitAll()
-                        .requestMatchers("/fotografos").hasRole("FOTOGRAFO")
-                        .requestMatchers("/clientes").hasRole("CLIENTE")
+
+                        // ROTAS DE FOTÓGRAFO
+                        .requestMatchers(HttpMethod.GET, "/fotografos/**").hasAnyRole("FOTOGRAFO", "CLIENTE")
+                        .requestMatchers(HttpMethod.POST, "/fotografos/**").hasRole("FOTOGRAFO")
+                        .requestMatchers(HttpMethod.PUT, "/fotografos/**").hasRole("FOTOGRAFO")
+                        .requestMatchers(HttpMethod.DELETE, "/fotografos/**").hasRole("FOTOGRAFO")
+
+                        // ROTAS DE CLIENTE
+                        .requestMatchers(HttpMethod.GET, "/clientes/**").hasAnyRole("FOTOGRAFO", "CLIENTE")
+                        .requestMatchers(HttpMethod.POST, "/clientes/**").hasRole("CLIENTE")
+                        .requestMatchers(HttpMethod.DELETE, "/clientes/**").hasRole("CLIENTE")
+                        .requestMatchers(HttpMethod.PUT, "/clientes/**").hasRole("CLIENTE")
+
+                        // ROTAS DE ALBUM
+                        .requestMatchers(HttpMethod.GET, "/album/**").hasAnyRole("FOTOGRAFO", "CLIENTE")
+                        .requestMatchers(HttpMethod.POST, "/album/**").hasRole("FOTOGRAFO")
+                        .requestMatchers(HttpMethod.DELETE, "/album/**").hasRole("FOTOGRAFO")
+                        .requestMatchers(HttpMethod.PUT, "/album/**").hasRole("FOTOGRAFO")
+
+                        //ROTAS DE FOTOS
+                        .requestMatchers(HttpMethod.GET, "/fotos/**").hasAnyRole("FOTOGRAFO", "CLIENTE")
+                        .requestMatchers(HttpMethod.POST, "/fotos/**").hasRole("FOTOGRAFO")
+                        .requestMatchers(HttpMethod.DELETE, "/fotos/**").hasRole("FOTOGRAFO")
+                        .requestMatchers(HttpMethod.PUT, "/fotos/**").hasRole("FOTOGRAFO")
+
+                        // ROTAS DE PAGAMENTOS
+                        // --
+
                         .anyRequest().authenticated()
                 )
                 .addFilter(new BasicAuthenticationFilter(authenticationManager(http)))
